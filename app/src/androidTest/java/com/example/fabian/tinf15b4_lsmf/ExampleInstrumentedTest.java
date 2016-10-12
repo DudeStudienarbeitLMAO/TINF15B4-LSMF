01fb14a373c573bc14c0491a6baf2f6a3c7fb59b
faxe1008 <vielspeicherplatz6@gmail.com>
2016-10-12 09:50:14 +0200

    multilanguage support test

--- app/src/androidTest/java/com/example/fabian/tinf15b4_lsmf/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/fabian/tinf15b4_lsmf/ExampleInstrumentedTest.java
@@ -22,5 +22,6 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.fabian.tinf15b4_lsmf", appContext.getPackageName());
+
     }
 }
--- app/src/main/java/com/example/fabian/tinf15b4_lsmf/MainActivity.java
+++ app/src/main/java/com/example/fabian/tinf15b4_lsmf/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.fabian.tinf15b4_lsmf;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import java.util.Locale;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+
+        String languageToLoad  = "de"; // your language
+        Locale locale = new Locale(languageToLoad);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config,
+                getBaseContext().getResources().getDisplayMetrics());
+        this.setContentView(R.layout.activity_main);
     }
+
+
 }
