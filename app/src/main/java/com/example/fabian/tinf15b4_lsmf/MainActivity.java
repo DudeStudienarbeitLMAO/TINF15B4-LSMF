254930d18943afce2619141835de24ae346fef24
Simon Gerhardt <s.gerhardt@seeburger.de>
2016-10-13 11:02:10 +0200

    Test-Commit

--- app/src/main/java/com/example/fabian/tinf15b4_lsmf/MainActivity.java
+++ app/src/main/java/com/example/fabian/tinf15b4_lsmf/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.fabian.tinf15b4_lsmf;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 import java.util.Locale;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        String languageToLoad  = "de"; // your language
+        String languageToLoad = "de"; // your language
         Locale locale = new Locale(languageToLoad);
         Locale.setDefault(locale);
         Configuration config = new Configuration();
@@ -26,7 +33,23 @@ public class MainActivity extends AppCompatActivity {
         getBaseContext().getResources().updateConfiguration(config,
                 getBaseContext().getResources().getDisplayMetrics());
         this.setContentView(R.layout.activity_main);
+        createTestView();
     }
 
 
+    public void createTestView() {
+        Button button = (Button) findViewById(R.id.button);
+        text = (TextView) findViewById(R.id.text);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (text != null) {
+                    text.setText("SUBBA");
+                }
+
+            }
+        });
+
+
+    }
 }
