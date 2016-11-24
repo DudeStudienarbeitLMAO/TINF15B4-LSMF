23ed34019262262e9690ffbdcece6707659681cf
faxe1008 <vielspeicherplatz6@gmail.com>
2016-11-24 18:10:12 +0100

    added validation logic for registration

--- app/src/main/java/com/example/fabian/tinf15b4_lsmf/register.java
+++ app/src/main/java/com/example/fabian/tinf15b4_lsmf/register.java
@@ -1,22 +1,95 @@
 package com.example.fabian.tinf15b4_lsmf;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.*;
+import android.support.v7.widget.*;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.*;
+import android.app.ProgressDialog;
 
 public class register extends AppCompatActivity {
+
+    EditText txt_username;
+    EditText txt_email;
+    EditText txt_password;
+    EditText txt_passwordconfirm;
+    AppCompatButton btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        txt_username = (EditText) findViewById(R.id.input_name);
+        txt_email = (EditText) findViewById(R.id.input_email);
+        txt_password = (EditText) findViewById(R.id.input_password);
+        txt_passwordconfirm = (EditText) findViewById(R.id.input_passwordconfirm);
+        btn_register = (AppCompatButton) findViewById(R.id.btn_register);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnregisterclick();
+            }
+        });
     }
 
-    public void navToLogin(View view)
-    {
+    public void navToLogin(View view) {
         Intent i = new Intent(getApplicationContext(), login.class);
         startActivity(i);
+    }
+
+    public void btnregisterclick() {
+
+        if (!isValidData()) {
+            Toast.makeText(getBaseContext(), getResources().getString(R.string.registerfailed), Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        btn_register.setEnabled(false);
+
+
+        String name = txt_username.getText().toString();
+        String email = txt_email.getText().toString();
+        String password = txt_password.getText().toString();
+
+
+        Toast.makeText(this, getResources().getString(R.string.registersuccess), Toast.LENGTH_LONG).show();
+
+        btn_register.setEnabled(true);
 
     }
+
+
+    public boolean isValidData() {
+        String name = txt_username.getText().toString();
+        String email = txt_email.getText().toString();
+        String password = txt_password.getText().toString();
+        String passwordconfirm = txt_passwordconfirm.getText().toString();
+
+        if (name.isEmpty() || name.length() < 6) {
+            txt_username.setError(getResources().getString(R.string.notenoughchars));
+            return false;
+        }
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            txt_email.setError(getResources().getString(R.string.invalidemail));
+            return false;
+        }
+
+        if (password.isEmpty() || password.length() < 6) {
+            txt_password.setError(getResources().getString(R.string.notenoughchars));
+            return false;
+        }
+
+        if (passwordconfirm.isEmpty() || !password.equals(passwordconfirm)) {
+            txt_password.setError(getResources().getString(R.string.invalidconfirm));
+            return false;
+        }
+
+        return true;
+    }
+
+
 }
